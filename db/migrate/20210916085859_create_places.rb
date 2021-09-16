5ae7be01c7455989d15b48debf00fbda6cadfdeb
Kurikoko <zomahyon@gmail.com>
2021-09-16 18:10:29 +0900

    仮フロント実装の為のview設置

--- db/migrate/20210916085859_create_places.rb
+++ db/migrate/20210916085859_create_places.rb
@@ -3,6 +3,8 @@ class CreatePlaces < ActiveRecord::Migration[6.0]
     create_table :places do |t|
 
       t.timestamps
+      t.string  :place_name, null: false
+      t.integer :area_id,    null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_080253) do
+ActiveRecord::Schema.define(version: 2021_09_16_085859) do
+
+  create_table "places", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "place_name", null: false
+    t.integer "area_id", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
