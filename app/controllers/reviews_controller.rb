d6aa3387c5673e16b7f3e473c914660cf28c17ed
Kurikoko <zomahyon@gmail.com>
2021-09-21 15:45:54 +0900

    編集処理実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
-  before_action :set_place_params, only:[:new, :create, :destroy]
+  before_action :set_place_params, only:[:new, :create, :edit, :destroy]
+  before_action :set_review_params, only: [:edit, :update, :destroy]
 
   def new
     @review = Review.new
@@ -13,9 +14,19 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-    review = Review.find(params[:id])
-    if review.destroy
+    if @review.destroy
       redirect_to place_path(@place)
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to place_path(@review.place_id)
+    else  
+      render :edit
     end
   end
 
@@ -26,5 +37,9 @@ class ReviewsController < ApplicationController
 
   def set_place_params
     @place = Place.find(params[:place_id])
+  end
+
+  def set_review_params
+    @review = Review.find(params[:id])
   end
 end
