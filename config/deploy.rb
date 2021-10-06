04ba7a0dd5796965865b1280e6377bd5ac6dda05
Kurikoko <zomahyon@gmail.com>
2021-10-06 20:50:46 +0900

    一部修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.16.0"
 
 set :application, "evels-tokyo"
-set :repo_url, "git@example.com:Kurikoko/evels-review.git"
+set :repo_url, "git@github.com:Kurikoko/evels-review.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
